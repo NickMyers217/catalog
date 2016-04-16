e64287cacb9bf5e720f28d248c40ea1d48de975b
Nickmyers217 <nickbmyers217@gmail.com>
2016-04-16 13:23:42 -0700

    Finished basic routing

--- routes.py
+++ routes.py
@@ -2,13 +2,29 @@
 
 from flask import Flask
 
+
+# Define the app
 app = Flask(__name__)
 
-# Route for the landing page
+
+# Route for the catalog page
 @app.route('/')
+@app.route('/catalog/')
 def landing():
     return 'Landing page'
 
+# Route to show a catedories items
+@app.route('/catalog/<category>/')
+@app.route('/catalog/<category>/items/')
+def items(category):
+    return 'Items for %s!' % category
 
+# Route to show a single item within a category
+@app.route('/catalog/<category>/<item>/')
+def itemDesc(category, item):
+    return 'Description page for %s in the %s category!' % (item, category)
+
+
+# Start the server
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8080, debug = True)
